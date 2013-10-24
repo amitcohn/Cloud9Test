c0db195d0e2f245f9d9364b17e70c5091fa58fc8
Ranen <raneng+bitbucket@gmail.com>
2013-10-24 17:39:26 -0400

    Plan for xug demo

---
+++ Plans/locinfo.js
@@ -0,0 +1,95 @@
+var http = require('http');
+//var httpsync = require('httpsync');
+
+function getRoute(zip1, zip2)
+{
+    // do the GET request
+    var httpsync = require('httpsync');
+    var url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + zip1 + "&destination=" + zip2 + "&sensor=false";
+    var req = httpsync.get({ url : url });
+    var resp = req.end();
+    var respJS = JSON.parse(resp.data);
+    //var addressFields = respJS['results'][0]['address_components'][1];
+    //city = respJS['results'][0]['address_components'][1]['long_name'];
+    //console.log(url);
+    //console.log(respJS);
+    var distanceMeters = respJS["routes"][0]["legs"][0]["distance"]["value"];
+    var distanceMiles = 0.00062137 * distanceMeters;
+    //console.log(zip1 + " --> " + zip2 + ": Distance: " + distanceMiles);
+    return distanceMiles;
+    //return city;
+}
+
+function getWeather(zip)
+{
+    var weathers = require('weathers');
+
+    weathers.getWeather("40.9530599", "-73.95354569999999", function(err, data){
+      var description = data["currentobservation"]["Weather"];
+      console.log(description);
+      return description;
+    });
+}
+
+function getcity(zip)
+{
+    // do the GET request
+    var resp = '';
+    var city = "";
+
+    var httpsync = require('httpsync');
+    var req = httpsync.get({ url : "http://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&sensor=true"});
+    var resp = req.end();
+    var respJS = JSON.parse(resp.data);
+    var addressFields = respJS['results'][0]['address_components'][1];
+    city = respJS['results'][0]['address_components'][1]['long_name'];
+    console.log(respJS);
+    return city;
+}
+
+function getcityAsync(zip)
+{
+    var optionsget = {
+        host : 'maps.googleapis.com', // here only the domain name
+        // (no http/https !)
+        port : 80,
+        path : '/maps/api/geocode/json?address=10017&sensor=true', // the rest of the url with parameters if needed
+    };
+    //http://maps.googleapis.com/maps/api/geocode/json?address=10017&sensor=true
+    //console.info('Options prepared:');
+    //console.info(optionsget);
+    //console.info('Do the GET call');
+    
+    // do the GET request
+    var resp = '';
+    var city = "Motza";
+    var reqGet = http.get(optionsget, function(res) {
+                               //console.log("statusCode: ", res.statusCode);
+                               // uncomment it for header details
+                               //	console.log("headers: ", res.headers);
+                               
+                               res.on('data', function(d) {
+                                      //console.info('GET result:\n');
+                                      //process.stdout.write(d);
+                                      //console.info('\n\nCall completed');
+                                      resp += d;
+                                      });
+                               
+                               res.on('end', function() {
+                                        var respJS = JSON.parse(resp);
+                                        city = respJS['results'][0]['address_components'][1]['long_name'];
+                                        console.log(city);
+                                        //return city;
+                                      });
+                               });
+    
+    return city;
+    reqGet.on('error', function(e) {
+              console.error(e);
+              });
+}
+
+module.exports.getcity = getcity;
+module.exports.getRoute = getRoute;
+module.exports.getWeather = getWeather;
+
--- Plans/sf.js
+++ Plans/sf.js
@@ -6,24 +6,86 @@ function PlanMod(plan)
 {
     this.plan1 = 10;
     this.plan = _plan = P = plan;
-    plan.addAdorAndDetails("FName", this.calc_FName, plan);
-    plan.addAdorAndDetails("LName", this.calc_LName, plan);
+    plan.addAdorAndDetails("FirstName", this.calc_FirstName, plan);
+    plan.addAdorAndDetails("LastName", this.calc_LastName, plan);
     plan.addAdorAndDetails("FullName", this.calc_FullName, plan);
+    plan.addAdorAndDetails("Company", this.calc_Company, plan);
+    plan.addAdorAndDetails("City", this.calc_City, plan);
+    plan.addAdorAndDetails("ZIP", this.calc_ZIP, plan);
+    plan.addAdorAndDetails("Temp", this.calc_Temp, plan);
+    plan.addAdorAndDetails("Distance", this.calc_Distance, plan);
+    plan.addAdorAndDetails("DistanceMsg", this.calc_DistanceMsg, plan);    
+
+    delete require.cache[require.resolve("./locinfo.js")];
 }
 
-PlanMod.prototype.calc_FName = function()
+PlanMod.prototype.calc_FirstName = function()
 {
     return P.FV('FirstName');
 }
 
-PlanMod.prototype.calc_LName = function()
+PlanMod.prototype.calc_LastName = function()
 {
     return P.FV('LastName');
 }
 
+PlanMod.prototype.calc_Company = function()
+{
+    return P.FV('Company');
+}
+
+PlanMod.prototype.calc_ZIP = function()
+{
+    return P.FV('PostalCode');
+}
+
+PlanMod.prototype.calc_City = function()
+{
+    return P.FV('City');
+}
+
 PlanMod.prototype.calc_FullName = function()
 {
-    return P.ADOR("FName") + " " + P.ADOR("LName");
+    return P.ADOR("FirstName") + " " + P.ADOR("LastName");
+}
+
+PlanMod.prototype.calc_Temp = function()
+{
+	var locinfo = require("./locinfo.js")
+    //return 72;//P.FV();
+    return locinfo.getWeather("07627");
+}
+
+PlanMod.prototype.calc_Distance = function()
+{
+	var xugHotelZip = "92802";
+	var recipientZip = P.ADOR("ZIP");
+	if (recipientZip == null)
+	{
+		return -1;
+	}
+	var locinfo = require("./locinfo.js")
+    var distance = locinfo.getRoute(recipientZip, xugHotelZip);
+    return Math.round(distance);
+}
+
+PlanMod.prototype.calc_DistanceMsg = function()
+{
+	var d = P.ADOR("Distance");
+	var msg;
+	if (d == -1)
+	{
+		msg = "Home is... somewhere"
+	}
+	else if (d < 100)
+	{
+		msg = "Home is close by";
+	}
+	else
+	{
+		msg = "Far from home";
+	}
+	return msg;
 }
 
 module.exports.PlanMod = PlanMod;
